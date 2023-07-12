1fd16208bf626faf42ce12c21f8ece28721ff4ce
Jailene <jaileneveras8@gmail.com>
2023-07-12 18:42:26 -0400

    moved and edited h2 in aboutme page

--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -9,12 +9,15 @@ function AboutMe() {
     <>
       <div className='border-boutme'>
         <p className='rectangle-text'>A lash technician with a passion for enhancing natural beauty, I am dedicated to providing exceptional lash services that leave my clients feeling confident and glamorous. Maintaining a clean and sanitary environment is also a priority, using high-quality products to ensure the health and safety of my clients. Book an appointment with me - let's enhance your natural beauty and elevate your lash game.</p>
+        <h2 className='sign'>Jessica Wong</h2>
       </div>
       <div className='image-and-signature'>
         <div className='oval-image'>
           <img className='selfie' src={selfie} alt='' />
+
         </div>
-        <h2 className='signature'>Jessica Wong</h2>
+       
+   
       </div>
     </>
   );
