0af24fd0151327476d3d0b93a2b9e606d7272333
Jailene <jaileneveras8@gmail.com>
2023-12-27 13:51:28 -0500

    added rewrites to package.json

--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -15,10 +15,10 @@ function AboutMe() {
     <div className='left' />
     <div className="right">
       <div className='content'>
-        <p className='description'>A lash technician based in South Florida with a passion for enhancing natural beauty, I am dedicated to providing exceptional lash services that leave my clients feeling confident. Maintaining a clean and sanitary environment is also a priority, using high-quality products to ensure the health and safety of my clients. Book an appointment with me - let's enhance your natural beauty and elevate your lash game! <br></br> 
+        <p className='description'>A lash technician based in South Florida with a passion for enhancing natural beauty, I am dedicated to providing exceptional lash services that leave my clients feeling confident. Maintaining a clean and sanitary environment is also a priority, using high-quality products to ensure the health and safety of my clients. Book an appointment with me - let's enhance your natural beauty and elevate your lash game <br></br> 
         </p>    
         <br></br>
-        <p className='sign'>Jessica Wong</p> 
+        <p className='sign'>Jessica Wong </p> 
         
                                                                     
 </div>
