66fb73c1c595bbdb7d1303ae4a3d44d4f97b9776
Jailene <jaileneveras8@gmail.com>
2024-01-02 13:31:10 -0500

    added picitures for gallery page

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -6,6 +6,10 @@ import { InlineWidget } from "react-calendly";
 import lash2 from '../assets/lash2.png';
 import lash3 from '../assets/lash3.png';
 import eyelash from '../assets/eyelash.png';
+import lash4 from '../assets/lash4.JPEG';
+import lash5 from '../assets/lash5.JPEG';
+import lash6 from '../assets/lash6.JPEG';
+
 
 const Slideshow = () => {
   return (
@@ -27,6 +31,27 @@ const Slideshow = () => {
       <div className="image-container">
         <img
           src={lash3}
+          className="image shadow-1-strong rounded mb-4"
+          alt="Boat on Calm Water"
+        />
+      </div>
+      <div className="image-container">
+        <img
+          src={lash4}
+          className="image shadow-1-strong rounded mb-4"
+          alt="Boat on Calm Water"
+        />
+      </div>
+      <div className="image-container">
+        <img
+          src={lash5}
+          className="image shadow-1-strong rounded mb-4"
+          alt="Boat on Calm Water"
+        />
+      </div>
+      <div className="image-container">
+        <img
+          src={lash6}
           className="image shadow-1-strong rounded mb-4"
           alt="Boat on Calm Water"
         />
