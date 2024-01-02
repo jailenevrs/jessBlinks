31a14164b50886ec3f4a0fb532e84d14cb1ef5f0
Jailene <jaileneveras8@gmail.com>
2024-01-02 13:56:47 -0500

    added max width to right container

--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -14,8 +14,8 @@ function AboutMe() {
      <div className='containers'>
     <div className='left' />
     <div className="right">
-      <div className='content'>
-        <p className='description'>A lash technician based in South Florida with a passion for enhancing natural beauty, I am dedicated to providing exceptional lash services that leave my clients feeling confident. Maintaining a clean and sanitary environment is also a priority, using high-quality products to ensure the health and safety of my clients. Book an appointment with me - let's enhance your natural beauty and elevate your lash game <br></br> 
+      <div className='boutme'>
+        <p className='description'>A lash technician based in South Florida with a passion for enhancing natural beauty, I am dedicated to providing exceptional lash services that leave my clients feeling confident. Book an appointment with me - let's enhance your natural beauty and elevate your lash game <br></br> 
         </p>    
         <br></br>
         <p className='sign'>Jessica Wong </p> 
@@ -24,9 +24,6 @@ function AboutMe() {
 </div>
 </div>
 </div>
-    <div className='oval-image'>
-        <img className='selfie src={selfie}'></img>   
-        </div>
   
     </>
   );
