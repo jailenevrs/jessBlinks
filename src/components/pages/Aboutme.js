1cfc405e7b2cd914caab0f459c60482b3e92a93a
Jailene <jaileneveras8@gmail.com>
2024-01-26 01:05:46 -0500

    media queries for description

--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -11,13 +11,12 @@ function AboutMe() {
   return (
     <>
    
-     <div className='containers'>
+     <div className='bout-me-container'>
     <div className='left' />
     <div className="right">
       <div className='boutme'>
         <p className='description'>A lash technician based in South Florida with a passion for enhancing natural beauty, I am dedicated to providing exceptional lash services that leave my clients feeling confident. Book an appointment with me - let's enhance your natural beauty and elevate your lash game <br></br> 
         </p>    
-        <br></br>
         <p className='sign'>Jessica Wong </p> 
         
                                                                     
