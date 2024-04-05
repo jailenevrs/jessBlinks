0bd161d9373b7e5d6b551c4708a056e0ae2e366b
Jailene <jaileneveras8@gmail.com>
2024-04-04 20:48:10 -0400

    changed css of about me page to make divs overlap

--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -10,19 +10,20 @@ import selfie from '../pages/selfie.jpeg';
 function AboutMe() {
   return (
     <>
-   
+    <section>
      <div className='bout-me-container'>
-    <div className='left' />
+    <div className='left'></div>
     <div className="right">
-      <div className='boutme'>
-        <p className='description'>A lash technician based in South Florida with a passion for enhancing natural beauty, I am dedicated to providing exceptional lash services that leave my clients feeling confident. Book an appointment with me - let's enhance your natural beauty and elevate your lash game <br></br> 
+      <div className='description'>
+        <p>A lash technician based in South Florida with a passion for enhancing natural beauty, I am dedicated to providing exceptional lash services that leave my clients feeling confident. Book an appointment with me - let's enhance your natural beauty and elevate your lash game <br></br> 
         </p>    
         <p className='sign'>Jessica Wong </p> 
-        
-                                                                    
+        </div>
+
+    </div>                                                            
 </div>
-</div>
-</div>
+</section>
+
   
     </>
   );
